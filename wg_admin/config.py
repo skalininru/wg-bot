cad33b70b0d01c79b795d553af40bda5ca589408
Sergey Kalinin <skalinin.info@gmail.com>
2022-08-04 15:46:45 +0300

    feat: added configuration check: all required env variables must be set

--- wg_admin/config.py
+++ wg_admin/config.py
@@ -1,13 +1,36 @@
 import os
 
+from loguru import logger
+
+required_variables = [
+    "BOT_API_TOKEN",
+    "WGA_DB_PASSWORD",
+    "WG_SERVER_PUBLIC_KEY",
+    "WG_SERVER_ENDPOINT",
+    "WG_ADMIN_USER"
+]
+
 LOG_LEVEL = os.getenv("WGA_LOG_LEVEL", "INFO")
+logger.debug("Init and check config")
+
+for var in required_variables:
+    if var not in os.environ:
+        err_message = (
+            "One or more requiered env variable is missing\n"
+            "Required variables:\n"
+            f"{required_variables}"
+        )
+        raise ValueError(err_message)
+logger.debug("Configuration initialized successfully")
 
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
-DB_HOST = os.getenv("WGA_DB_HOST")
-DB_NAME = os.getenv("WGA_DB_NAME")
-DB_USER = os.getenv("WGA_DB_USER")
+DB_HOST = os.getenv("WGA_DB_HOST", "127.0.0.1")
+DB_NAME = os.getenv("WGA_DB_NAME", "wga")
+DB_USER = os.getenv("WGA_DB_USER", "u_wga")
 DB_PASSWORD = os.getenv("WGA_DB_PASSWORD")
+
+ADMIN_USER = os.getenv("WG_ADMIN_USER")
 
 SERVER_PUBLIC_KEY = os.getenv("WG_SERVER_PUBLIC_KEY")
 SERVER_DNS = os.getenv("WG_SERVER_DNS", "1.1.1.1")
